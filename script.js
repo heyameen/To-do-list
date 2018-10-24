b49d1239b35dbaf4275cc7e368073e5135b4e5de
alade ameen <aladeameen@gmail.com>
2018-10-24 21:22:34 +0100

    ui element for todo-list

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filterTask = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+loadAllEventListeners();
+
+function loadAllEventListeners(){
+    
+}
